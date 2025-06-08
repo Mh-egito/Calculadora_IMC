e752215207271d1fb6a29a7ef8d5629028a8568f
Mh-egito <matheushegito@gmail.com>
2025-06-08 13:57:05 -0300

    teste do git 2

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+
+#importanto a biblioteca do tkinter para poder criar uma janela
+
+janela = Tk()
+janela.title('')
+janela.geometry('295x230')
+janela.configure(bg='white')
+
+janela.mainloop()
+
+#criação da janela realizada 
+
